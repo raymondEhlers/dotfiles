a809f46feb8a34ca9b29dc84f9a5041940387e15
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2013-03-20 03:41:41 -0400

    Begin to add Unit testing and centralize autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+if [[ -z "$@" ]]; then
+	configureVars="--prefix=$MYINSTALL"
+else
+	configureVars="$@"
+fi
+
+echo "Configure parameters: $configureVars"
+echo "Running in directory: $PWD"
+
+#(cd $srcdir; aclocal -I ${OFFLINE_MAIN}/share;\
+(cd $srcdir; aclocal; libtoolize --force; automake -a --add-missing; autoconf)
+
+$srcdir/configure "$configureVars" 
+
--- saveDisplay.sh
+++ saveDisplay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$DISPLAY" > .dispName.txt
+echo "$DISPLAY" > ~/.dispName.txt
 echo "$DISPLAY"
